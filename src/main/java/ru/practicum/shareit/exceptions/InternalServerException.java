3ea5569033630a8700b345ffa04a9c5c500f8f8d
Sem Lyakin <xsemx@yandex.ru>
2024-10-24 12:59:06 +0400

    Спринт 16. Разделен на модули

--- src/main/java/ru/practicum/shareit/exceptions/InternalServerException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-public class InternalServerException extends RuntimeException {
-    public InternalServerException(String message) {
-        super(message);
-    }
-}
